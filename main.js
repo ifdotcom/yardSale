f9acf6c95fbacf1d778a658474581c4f73394359
Fernanda Vidal <ifervb@gmail.com>
2022-08-13 21:57:17 -0500

    Render de productos

--- main.js
+++ main.js
@@ -23,3 +23,82 @@ function toggleMenuCart() {
   menuMobile.classList.add("inactive");
   menuDesktop.classList.add("inactive");
 }
+
+// productos
+
+const arrProducts = [];
+
+arrProducts.push({
+  nameProduct: "Cactus",
+  imgProduct:
+    "https://images.pexels.com/photos/5783396/pexels-photo-5783396.jpeg?cs=srgb&dl=pexels-anna-khomutova-5783396.jpg&fm=jpg",
+  priceProduct: 150,
+});
+arrProducts.push({
+  nameProduct: "Bamboo",
+  imgProduct:
+    "https://images.pexels.com/photos/5783396/pexels-photo-5783396.jpeg?cs=srgb&dl=pexels-anna-khomutova-5783396.jpg&fm=jpg",
+  priceProduct: 250,
+});
+arrProducts.push({
+  nameProduct: "Suculenta",
+  imgProduct:
+    "https://images.pexels.com/photos/5783396/pexels-photo-5783396.jpeg?cs=srgb&dl=pexels-anna-khomutova-5783396.jpg&fm=jpg",
+  priceProduct: 350,
+});
+arrProducts.push({
+  nameProduct: "Cactus",
+  imgProduct:
+    "https://images.pexels.com/photos/5783396/pexels-photo-5783396.jpeg?cs=srgb&dl=pexels-anna-khomutova-5783396.jpg&fm=jpg",
+  priceProduct: 150,
+});
+arrProducts.push({
+  nameProduct: "Bamboo",
+  imgProduct:
+    "https://images.pexels.com/photos/5783396/pexels-photo-5783396.jpeg?cs=srgb&dl=pexels-anna-khomutova-5783396.jpg&fm=jpg",
+  priceProduct: 250,
+});
+arrProducts.push({
+  nameProduct: "Suculenta",
+  imgProduct:
+    "https://images.pexels.com/photos/5783396/pexels-photo-5783396.jpeg?cs=srgb&dl=pexels-anna-khomutova-5783396.jpg&fm=jpg",
+  priceProduct: 350,
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const cardContainer = document.querySelector(".cards-container");
+
+    const cardProduct = document.createElement("div");
+    cardProduct.classList.add("card");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", product.imgProduct);
+
+    const cardInfo = document.createElement("div");
+
+    cardInfo.classList.add("card-items");
+    const info = document.createElement("div");
+
+    const price = document.createElement("p");
+    price.innerText = `$${product.priceProduct}`;
+    const name = document.createElement("p");
+    name.innerText = `${product.nameProduct}`;
+
+    const icon = document.createElement("figure");
+
+    const iconCart = document.createElement("img");
+    iconCart.setAttribute("src", "./Icons/bt_add_to_cart.svg");
+
+    cardContainer.appendChild(cardProduct);
+    cardProduct.appendChild(img);
+    cardProduct.appendChild(cardInfo);
+    cardInfo.appendChild(info);
+    cardInfo.appendChild(icon);
+    info.appendChild(price);
+    info.appendChild(name);
+    icon.appendChild(iconCart);
+  }
+}
+
+renderProducts(arrProducts);
